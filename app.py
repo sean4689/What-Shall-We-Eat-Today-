95b81f5737a4a5a4d38fa6b39a1f3f4dd023b533
梁定煊 <10813009@g.clvs.tyc.edu.tw>
2023-11-30 09:22:00 +0800

    可輸入地址版

--- app.py
+++ app.py
@@ -3,7 +3,12 @@ import findfood as fd
 app = Flask(__name__)
 @app.route("/",methods=['POST','GET'])
 def go():
-    a=fd.suffer()
-    return render_template('index.html',key=a[0],img=a[1])
+    if request.method=="POST":
+        a=request.form['position']
+        fdata=fd.finddata(1,str(a))
+        fd.wwrite(fdata)
+        a=fd.suffer()
+        return render_template('index.html',key=a[0],img=a[1])
+    return render_template('index.html')
 if __name__ == '__main__':
     app.run(debug=1) 
